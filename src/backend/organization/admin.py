760b098ab913e4773063e17cc5b02971b8933056
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-10-25 03:50:32 -0300

    improved admin model registration code

--- src/backend/organization/admin.py
+++ src/backend/organization/admin.py
@@ -1,31 +1,17 @@
 from django.contrib import admin
 
+from src.backend.core.admin import BaseAdmin
 from src.backend.organization.models import Membership, Organization, AcademicClasses
 
 # Register your models here.
 @admin.register(Membership)
-class MembershipAdmin(admin.ModelAdmin):
-    fields = [
-        "user",
-        "organization",
-        "role",
-        "is_active"
-    ]
+class MembershipAdmin(BaseAdmin):
+    fields = ["user", "organization", "role", "is_active"]
 
 @admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    fields = [
-        "name",
-        "description"
-    ]
+class OrganizationAdmin(BaseAdmin):
+    fields = ["name", "description"]
 
 @admin.register(AcademicClasses)
-class AcademicClassesAdmin(admin.ModelAdmin):
-    fields = [
-        "teacher",
-        "students",
-        "organization",
-        "created_by",
-        "created_at",
-        "deleted_at"
-    ]
+class AcademicClassesAdmin(BaseAdmin):
+    fields = ["teacher", "students", "organization"]
--- src/backend/words_soup_game/admin.py
+++ src/backend/words_soup_game/admin.py
@@ -1,49 +1,20 @@
 from django.contrib import admin
+from src.backend.core.admin import BaseAdmin
 from src.backend.words_soup_game.models import Exercise, Word, exercise_record, exercise_schedule
 
-@admin.register(Exercise)
-class ExerciseAdmin(admin.ModelAdmin):
-    fields = [
-        "correct_word",
-        "wrong_words",
-        "is_public",
-        "organization",
-        "created_by",
-        "created_at",
-        "updated_at",
-        "deleted_at"
-        
-    ]
-
 @admin.register(Word)
-class WordAdmin(admin.ModelAdmin):
-    fields = [
-        "name",
-        "created_at",
-        "updated_at",
-        "deleted_at"
-    ]
+class WordAdmin(BaseAdmin):
+    fields = ["name"]
 
-@admin.register(exercise_record)
-class ExerciseRecordAdmin(admin.ModelAdmin):
-    fields = [
-        "exercise",
-        "organization",
-        "schedule",
-        "created_by",
-        "created_at",
-        "updated_at",
-        "deleted_at"
-    ]
+@admin.register(Exercise)
+class ExerciseAdmin(BaseAdmin):
+    fields = ["correct_word", "wrong_words", "is_public", "organization", "created_by"]
+
 
 @admin.register(exercise_schedule)
-class ExeciseSchedule(admin.ModelAdmin):
-    fields = [
-        "exercise",
-        "academic_classes",
-        "organization",
-        "deadline",
-        "created_at",
-        "updated_at",
-        "deleted_at"
-    ]
+class ExeciseSchedule(BaseAdmin):
+    fields = ["exercise", "academic_classes", "organization", "deadline", "created_by"]
+
+@admin.register(exercise_record)
+class ExerciseRecordAdmin(BaseAdmin):
+    fields = ["exercise", "organization", "schedule", "created_by"]
