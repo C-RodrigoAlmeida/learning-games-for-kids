d592597e6c59416bfa96a27d2dd0651c87847333
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-10-24 23:46:51 -0300

    added ExerciseRecords, ExerciseSchedule

--- src/backend/words_soup_game/admin.py
+++ src/backend/words_soup_game/admin.py
@@ -1,19 +1,49 @@
 from django.contrib import admin
-from src.backend.words_soup_game.models import Exercise, Word
+from src.backend.words_soup_game.models import Exercise, Word, exercise_record, exercise_schedule
 
 @admin.register(Exercise)
 class ExerciseAdmin(admin.ModelAdmin):
     fields = [
         "correct_word",
         "wrong_words",
-        'is_public',
-        'organization',
-        'created_by',
+        "is_public",
+        "organization",
+        "created_by",
+        "created_at",
+        "updated_at",
+        "deleted_at"
         
     ]
 
 @admin.register(Word)
 class WordAdmin(admin.ModelAdmin):
     fields = [
-        'name'
+        "name",
+        "created_at",
+        "updated_at",
+        "deleted_at"
+    ]
+
+@admin.register(exercise_record)
+class ExerciseRecordAdmin(admin.ModelAdmin):
+    fields = [
+        "exercise",
+        "organization",
+        "schedule",
+        "created_by",
+        "created_at",
+        "updated_at",
+        "deleted_at"
+    ]
+
+@admin.register(exercise_schedule)
+class ExeciseSchedule(admin.ModelAdmin):
+    fields = [
+        "exercise",
+        "academic_classes",
+        "organization",
+        "deadline",
+        "created_at",
+        "updated_at",
+        "deleted_at"
     ]
