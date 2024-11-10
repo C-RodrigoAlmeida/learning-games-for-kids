3bc4539e8e7997edcbb6796c2081a0142614a89e
Robbobs <rendash0@gmail.com>
2024-11-09 23:18:34 -0300

    created PlayComponent

--- src/frontend/app/app.routes.ts
+++ src/frontend/app/app.routes.ts
@@ -10,61 +10,30 @@ import { AcademicClassesListComponent } from "./organization/academic-classes/ac
 import { ExerciseRecordListComponent } from "./words-soup-game/exercises/exercise-record/exercise-record-list/exercise-record-list.component";
 import { ExerciseScheduleListComponent } from "./words-soup-game/exercises/exercise-schedule/exercise-schedule-list/exercise-schedule-list.component";
 import { WordFormComponent } from "./words-soup-game/words/word-form/word-form.component";
+import { PlayComponent } from "./words-soup-game/pages/play/play.component";
+
 export const routes: Routes = [
-    {
-        path: '',
-        component: AppComponent
-    },
+    { path: '', component: AppComponent },
     {
         path: 'home',
         loadComponent: () => import('./nav/home/home.component')
             .then(m => m.HomeComponent)
     },
     // TODOS ESTES ABAIXO SÃO PARA TESTES, DEPOIS REMOVER:
-    {
-        path: 'word',
-        component: WordFormComponent
-    },
-    {
-        path: 'word/:id',
-        component: WordFormComponent
-    },
-    {
-        path: 'words',  
-        component: WordListComponent
-    },
-    {
-        path: 'exercise-schedules',
-        component: ExerciseScheduleListComponent
-    },
-    {
-        path: 'exercise-records',
-        component: ExerciseRecordListComponent
-    },
-    {
-        path: 'exercises',
-        component: ExerciseListComponent
-    },
-    {
-        path: 'organizations',
-        component: OrganizationListComponent
-    },
-    {
-        path: 'memberships',
-        component: MembershipListComponent
-    },
-    {
-        path: 'academic-classes',
-        component: AcademicClassesListComponent
-    },
- 
+    { path: 'word', component: WordFormComponent },
+    { path: 'word/:id', component: WordFormComponent },
+    { path: 'words', component: WordListComponent },
+    { path: 'exercise-schedules', component: ExerciseScheduleListComponent },
+    { path: 'exercise-records', component: ExerciseRecordListComponent },
+    { path: 'exercises', component: ExerciseListComponent },
+    { path: 'organizations', component: OrganizationListComponent },
+    { path: 'memberships', component: MembershipListComponent },
+    { path: 'academic-classes', component: AcademicClassesListComponent },
+    { path: '**', component: LoginComponent }, // mudar para 404 not found
+    { path: 'play', component: PlayComponent }
     // {
     //     path: 'account',
     //     loadChildren: () => import('./accounts/login/login.component')
     //         .then(m => m.LoginComponent)
     // },
-    {
-        path: '**',
-        component: LoginComponent // mudar para 404 not found
-    },
 ]
