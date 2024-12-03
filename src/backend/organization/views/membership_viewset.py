27ddf57068b7c8da9776f56213f3ffa62b0e2b50
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-12-03 11:49:23 -0300

    adding route to check organization a member is part of

--- src/backend/organization/views/membership_viewset.py
+++ src/backend/organization/views/membership_viewset.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from src.backend.organization.models import Membership
 from src.backend.organization.serializers import MembershipSerializer, MembershipCreateSerializer
+from src.backend.organization.serializers.organization_serializer import OrganizationSerializer
 
 __all__ = ["MembershipViewSet"]
 
@@ -17,4 +20,13 @@ class MembershipViewSet(viewsets.ModelViewSet):
         if self.action == 'create': 
             return MembershipCreateSerializer
         
+        if self.action == 'organizations':
+            return OrganizationSerializer
+        
         return MembershipSerializer
+    
+    @action(detail=False, methods=['get'])
+    def organizations(self) -> Response:
+        queryset = Membership.objects.select_related('organization').filter(user=self.request.user)
+        serializer: OrganizationSerializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
